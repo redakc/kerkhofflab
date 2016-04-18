5d88fb4fbfc50d568345b9f72451483077dc9466
redakc <redakc@kenyon.edu>
2016-04-17 22:46:44 -0400

    Vector Codes
    R code for clade vectors

---
+++ Vector Code.R
@@ -0,0 +1,102 @@
+#Asimina
+Asiminavector=c("Asimina")
+AsiminaGIS=BIEN.gis.genus(Asiminavector)
+
+#Ptelea
+Pteleavector=c("Ptelea")
+PteleaGIS=BIEN.gis.genus(Pteleavector)
+
+#Hypericum
+Hypericumvector=c("Hypericum")
+HypericumGIS=BIEN.gis.genus(Hypericumvector)
+
+#Prunus 1
+Prunus1vector=c("Prunus americana","Prunus angustifolia","Prunus hortulana","Prunus munsonia")
+Prunus1GIS = BIEN.gis.species(Prunus1vector)
+
+#Sideroxylon
+Sideroxylonvector=c("Sideroxylon")
+SideroxylonGIS=BIEN.gis.genus(Sideroxylonvector)
+
+
+#Juglans 2
+Juglans2vector=c("Juglans nigra")
+Juglans2GIS=BIEN.gis.species(Juglans2vector)
+
+
+#Ilex 3
+Ilex3vector=c("Ilex opaca")
+Ilex3GIS=BIEN.gis.species(Ilex3vector)
+
+#Ceanothus
+Ceanothusvector=c("Ceanothus")
+CeanothusGIS=BIEN.gis.genus(Ceanothusvector)
+
+
+#Gaylussacia 2 
+Gaylussacia2vector=c("Gaylussacia dumosa")
+Gaylussacia2GIS=BIEN.gis.species(Gaylussacia2vector)
+
+#Quercus 2
+Quercus2vector=c("Quercus coccinea","Quercus falcata","Quercus imbricaria","Quercus marilandica","Quercus nigra","Quercus phellos","Quercus rubra","Quercus shumardii","Quercus velutina")
+Quercus2GIS=BIEN.gis.species(Quercus2vector)
+
+
+#Robinia
+Robiniavector=c("Robinia")
+RobiniaGIS=BIEN.gis.genus(Robiniavector)
+
+#Eubotrys
+Eubotrysvector=c("Eubotrys")
+EubotrysGIS=BIEN.gis.genus(Eubotrysvector)
+
+
+#Fothergilla
+Fothergillavector=c("Fothergilla")
+FothergillaGIS=BIEN.gis.genus(Fothergillavector)
+
+
+#Gaylussacia 1
+Gaylussacia1vector=c("Gaylussacia baccata","Gaylussacia tomentosa","Gaylussacia ursina")
+Gaylussacia1GIS=BIEN.gis.species(Gaylussacia1vector)
+
+
+#Hudsonia
+Hudsoniavector=c("Hudsonia")
+HudsoniaGIS=(Hudsoniavector)
+
+
+#Ilex1
+Ilex1vector=c(“Ilex collina”, “Ilex montana”)
+Ilex1GIS=BIEN.gis.species(Ilex1vector
+                          
+                          
+#Kalmia1a
+Kalmia1avector=c(“Kalmia latifolia”)
+Kalmia1aGIS=BIEN.gis.species.(Kalmia1avector)
+                          
+                          
+#Paxistima
+Paxistimavector=c(“Paxistima)
+PaxistimaGIS=BIEN.gis.genus(Paxistimavector)
+                          
+#Sambucus1
+Sambucus1vector=c(“Sambucus canadensis”)
+Sambucus1GIS=BIEN.gis.species(“Sambucus1vector)
+                          
+                          
+#Vitis2
+Vitis2vector=c(“Vitis rotundifolia”)Vitis2GIS=BIEN.gis.species(Vitis2vector)
+                          
+#Dirca
+Dircavector=c(“Dirca”)
+DircaGIS=BIEN.gis.genus(Dircavector)
+                          
+#Ulmus2
+Ulmus2vector=c(“Ulmus alata”)
+Ulmus2GIS=BIEN.gis.species(Ulmus2vector)
+                          
+                          
+                          
+
+
